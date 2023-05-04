d762a3a6f41de788e17ceee4fb92e24fb3c96248
Cathrynlyonss <73148250+Cathrynlyonss@users.noreply.github.com>
2023-05-04 12:31:11 -0500

    Add files via upload

--- rsspmm.cu
+++ rsspmm.cu
@@ -8,50 +8,46 @@ int IN_TILE_SLICE_SIZE = 32;
 int WARP_SIZE = 32;
 int tid = threadIdx.x;
 int tb_idx = blockIdx.x;
-int start = 0;
-int end = 0;
-int row_offset, slice_offset, warp_id, lane_id, i;
 [int][int] sm_input_value[IN_TILE_ROW_SIZE][WARP_SIZE];
 [int][int] input_value[IN_TILE_ROW_SIZE][IN_TILE_SLICE_SIZE];
 int seg_start_num[IN_TILE_ROW_SIZE];
 int start_seg_position[IN_TILE_ROW_SIZE];
-int index_buf, value_buf, val, mod, row_idx;
+int index_buf, value_buf, mod;
 int seg_index[IN_TILE_ROW_SIZE];
 
 
 //Heavy Row Segments
 void heavy(){
-	row_offset = tb_idx * IN_TILE_ROW_SIZE ;
-	slice_offset = tb_idy * IN_TILE_SLICE_SIZE ;
-	warp_id = tid / WARP_SIZE ;
-	lane_id = tid % WARP_SIZE ;
+	int row_offset = tb_idx * IN_TILE_ROW_SIZE ;
+	int slice_offset = tb_idy * IN_TILE_SLICE_SIZE ;
+	int warp_id = tid / WARP_SIZE ;
+	int lane_id = tid % WARP_SIZE ;
 
-	for i = warp_id to IN_TILE_ROW_SIZE step tb . size ()/ WARP_SIZE do
+	for(int i = warp_id; i <=  IN_TILE_ROW_SIZE; i += (tb . size ()/ WARP_SIZE)){
 		sm_input_value [ i ][ lane_id ] = input_value [ row_offset +i ][ slice_offset + lane_id ];
-	end
+	}
 
 	__syncthreads ;
 
-	for i = seg_start_num [ tb_idx ] to seg_start_num [ tb_idx +1] -1 step tb.size ()/ WARP_SIZE do
-		val = 0;
-		start = start_seg_position [i ];
-		end = start_seg_position [i +1];
+	for(int i = seg_start_num [ tb_idx ]; i <= (seg_start_num [ tb_idx +1] -1); i+= (tb.size ()/ WARP_SIZE)){
+		int val = 0;
+		int start = start_seg_position [i ];
+		int end = start_seg_position [i +1];
 
-		for j = start to end -1 do
+		for(int j = start; j <= (end -1); j++){
+		
 			mod = ( j - start )% WARP_SIZE
-
-			if mod == 0 then
-				index_buf = seg_index [ j + lane_id ];
-				value_buf = seg_value [ j + lane_id ];
-			end
+			index_buf = seg_index [ j + lane._id ];
+			value_buf = seg_value [ j + lane_id ];
+		}
 			val += sm_input_value [ __shfl ( index_buf , mod )][ lane_id ]
 			* __shfl ( value_buf , mod );
-		end
+	}
 		
-		row_idx = seg_row_position [i ];
-		// directly accumulate results in global memory
-		atomicAdd (& dest_value [ row_idx ][ slice_offset + lane_id ] , val );
-	end
+	int row_idx = seg_row_position [i];
+	// directly accumulate results in global memory
+	atomicAdd (& dest_value [ row_idx ][ slice_offset + lane_id ] , val );
+	
 }
 
 
@@ -60,21 +56,21 @@ void heavy(){
 //light rows 
 void light(){
 
-	row_offset = ( tb_idx * tb . size () + tid ) / WARP_SIZE ;
-	slice_offset = tb_idy * IN_TILE_COL_SIZE ;
-	lane_id = tid % WARP_SIZE ;
-	start = csr_row_pointer [ row_offset ];
-	end = csr_row_pointer [ row_offset +1];
-	val = 0;
-	for i = start to end -1 do
+	int row_offset = ( tb_idx * tb . size () + tid ) / WARP_SIZE ;
+	int slice_offset = tb_idy * IN_TILE_COL_SIZE ;
+	int lane_id = tid % WARP_SIZE ;
+	int start = csr_row_pointer [ row_offset ];
+	int end = csr_row_pointer [ row_offset +1];
+	int val = 0;
+	for(int i = start; i <= (end -1); i++){
 		mod = ( i - start )% WARP_SIZE
-		if mod == 0 then
+		if(mod == 0){
 			index_buf = csr_column_idx [ i + lane_id ];
 			value_buf = csr_column_val [ i + lane_id ];
-		end
+		}
 		val += input_value [ __shfl ( index_buf , mod )][ lane_id ]
 		* __shfl ( value_buf , mod );
-	end
+	}
 	// directly accumulate results in global memory
 	atomicAdd (& dest_value [ row_offset ][ slice_offset + lane_id ] , val );
 }
